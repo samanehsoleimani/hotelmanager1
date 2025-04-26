2073a63db9619ac7d44a353f49dbda13c9ca82e8
samanehsoleimani <soleimanisaman08@gmail.com>
2025-04-26 19:58:01 +0330

    ایجاد پروژه اولیه

---
+++ HOTELPROJECT/src/txtfileManager/Main.java
@@ -0,0 +1,33 @@
+package txtfileManager;
+
+import Commons.Guests;
+import Manager.GuestsManager;
+
+public class Main {
+    public static void main(String[] args) {
+        GuestsManager manager = new GuestsManager();
+
+        // ساخت یک مهمان
+        Guests guest = new Guests();
+        guest.setFullName("Ali Rezaei");
+        guest.setMellicode(123456789);
+        guest.setPhoneNumber("09123456789");
+        guest.setRoomNumber(101);
+
+        // اضافه کردن مهمان
+       // manager.insert(guest);
+        System.out.println("✅ Guest inserted.");
+
+        // نمایش همه مهمان‌ها
+        //System.out.println("\n📋 List of all guests:");
+        //manager.listAllGuests();
+        manager.deleteGuestByName("Ali Rezaei");
+        // پیدا کردن یک مهمان
+        Guests foundGuest = manager.getGuestByName("Ali Rezaei");
+        if (foundGuest != null) {
+            System.out.println("\n🔍 Guest found: " + foundGuest.getFullName() + ", Room: " + foundGuest.getRoomNumber());
+        } else {
+            System.out.println("\n❌ Guest not found.");
+        }
+    }
+}
