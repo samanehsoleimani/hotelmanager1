4e62ea4e062913a1ab0e2bf6e8d2e68a7545a653
Maryam Ghorbani <mrymgh635@gmail.com>
2025-04-29 15:53:14 +0330

    افزودن فایل دوره کارکنان4

---
+++ HOTELPROJECT/src/Commons/Registration.java
@@ -0,0 +1,42 @@
+package common;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Registration implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String fullName;
+    private String nationalId;
+    private String phone;
+    private String courseName;
+    private int coursePrice;
+    private String courseSchedule;
+    private String courseDate;
+    private String registrationDate;
+
+    public Registration(String fullName, String nationalId, String phone,
+                        String courseName, int coursePrice,
+                        String courseSchedule, String courseDate) {
+        this.fullName = fullName;
+        this.nationalId = nationalId;
+        this.phone = phone;
+        this.courseName = courseName;
+        this.coursePrice = coursePrice;
+        this.courseSchedule = courseSchedule;
+        this.courseDate = courseDate;
+        this.registrationDate = new SimpleDateFormat("yyyy/MM/dd HH:mm").format(new Date());
+    }
+
+    public String getRegistrationInfo() {
+        return "نام و نام خانوادگی: " + fullName +
+                "\nکد ملی: " + nationalId +
+                "\nشماره تماس: " + phone +
+                "\nدوره: " + courseName +
+                "\nزمان برگزاری: " + courseSchedule +
+                "\nتاریخ برگزاری: " + courseDate +
+                "\nهزینه: " + Utils.formatPrice(coursePrice) +
+                "\nتاریخ ثبت‌نام: " + registrationDate;
+    }
+}
